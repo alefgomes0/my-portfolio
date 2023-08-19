80086c808c805e83404bcc51027dc1b7372606f8
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-08-19 00:46:44 -0300

    Faz pequenos ajustes

--- src/components/NightMode/NightMode.tsx
+++ src/components/NightMode/NightMode.tsx
@@ -19,7 +19,7 @@ export const NightMode = (props: NightModeProps) => {
           props.theme === "light"
             ? "opacity-100"
             : "opacity-50"
-        } transition-opacity duration-300`}
+        } transition-opacity transition-colors duration-300`}
       >
         <Sun width={props.width} height={props.height} />
       </div>
@@ -29,7 +29,7 @@ export const NightMode = (props: NightModeProps) => {
           props.theme === "dark"
             ? "opacity-100"
             : "opacity-50"
-        } transition-opacity duration-300`}
+        } transition-opacity transition-colors duration-300`}
       >
         <Moon width={props.width} height={props.height} />
       </div>
