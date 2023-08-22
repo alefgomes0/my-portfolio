0cfe85d8810a25dcc2178dabcea5d7ec879d15e3
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-08-21 22:17:48 -0300

    Adiciona mais projetos antigos

--- src/components/Projetos/Projetos.tsx
+++ src/components/Projetos/Projetos.tsx
@@ -153,13 +153,19 @@ export const Projetos = () => {
               <span className="font-[metropolisbold] dark:text-white">
                 props
               </span>{" "}
-              e <span className="font-[metropolisbold] dark:text-white">hooks</span> foram
-              utilizados em todo o projeto.
+              e{" "}
+              <span className="font-[metropolisbold] dark:text-white">
+                hooks
+              </span>{" "}
+              foram utilizados em todo o projeto.
             </h5>
           </div>
         </div>
         <Link to="/projetos">
-          <button className="text-xl text-slate-50 ml-8 mb-8 md:ml-24 bg-cyan-600 px-4 py-1 rounded shadow-[0_3px_3px_0_rgba(0,0,0,0.25)] hover:shadow-[0_3px_3px_0_rgba(0,0,0,0.25)_inset]">
+          <button
+            className="text-xl text-slate-50 ml-8 mb-8 md:ml-24 bg-cyan-600 px-4 py-1 rounded shadow-[0_3px_3px_0_rgba(0,0,0,0.25)] hover:shadow-[0_3px_3px_0_rgba(0,0,0,0.25)_inset]"
+            onClick={() => window.scrollTo({ top: 0, behavior: "instant" })}
+          >
             VER MAIS PROJETOS
           </button>
         </Link>
--- src/pages/Projetos/TodosProjetos.tsx
+++ src/pages/Projetos/TodosProjetos.tsx
@@ -8,7 +8,7 @@ import { TypeScriptColorido } from "../../components/svg/TypeScript";
 
 export const TodosProjetos = () => {
   return (
-    <div className="dark:text-neutral-100 dark:bg-neutral-900 font-[metropolisregular] grid grid-cols-1 grid-rows-[auto_1fr] gap-2 pt-24 relative">
+    <div className="dark:text-neutral-100 dark:bg-neutral-900 bg-neutral-100 font-[metropolisregular] grid grid-cols-1 grid-rows-[auto_1fr] gap-2 pt-24 relative">
       <h2 className="dark:text-neutral-50 font-[metropolisbold] opacity-80 text-4xl md:text-6xl px-8 md:px-24 mb-8">
         Meus Projetos
       </h2>
@@ -189,7 +189,58 @@ export const TodosProjetos = () => {
         </div>
         <div className="dark:bg-neutral-800 grid grid-cols-1 md:grid-cols-2 grid-rows-[auto_1fr] gap-x-2 md:gap-x-8 gap-y-2 bg-neutral-50 px-8 md:px-24 py-8">
           <h3 className="row-start-1 row-end-2 col-start-1 col-end-2 text-3xl">
-            Admin Dashboard
+            Jogo da Memória
+          </h3>
+          <div className="h-fit">
+            <a href="https://memory-card-five-zeta.vercel.app/" target="_blank">
+              <img
+                className="min-h-[140px] min-w-[280px] object-center object-cover rounded shadow-[0_3px_3px_0_rgba(0,0,0,0.25)]"
+                srcSet="
+                  /imagens/mc.png 600w,
+                "
+              />
+            </a>
+          </div>
+          <div className="flex flex-col self-start md:col-start-2 md:col-end-3 md:row-start-1 md:row-end-3 gap-4">
+            <h4 className="text-xl pt-3">PRINCIPAIS TECNOLOGIAS</h4>
+            <div className="flex gap-6">
+              <ReactColorido width={60} height={60} />
+              <CSSColorido width={60} height={60} />
+              <JavaScript width={60} height={60} />
+              <GitColorido width={60} height={60} />
+            </div>
+          </div>
+        </div>
+        <div className="dark:bg-zinc-800 bg-slate-200 grid grid-cols-1 md:grid-cols-2 grid-rows-[auto_1fr] gap-x-2 md:gap-x-8 gap-y-2 bg-neutral-50 px-8 md:px-24 py-8">
+          <h3 className="row-start-1 row-end-2 col-start-1 col-end-2 text-3xl">
+            Lista de Afazeres
+          </h3>
+          <div className="h-fit">
+            <a href="https://illustrious-churros-02bc2b.netlify.app/" target="_blank">
+              <img
+                className="min-h-[140px] min-w-[280px] object-center object-cover rounded shadow-[0_3px_3px_0_rgba(0,0,0,0.25)]"
+                srcSet="
+                  /imagens/td.png 600w,
+                "
+              />
+            </a>
+          </div>
+          <div className="flex flex-col self-start md:col-start-2 md:col-end-3 md:row-start-1 md:row-end-3 gap-4">
+            <h4 className="text-xl pt-3">PRINCIPAIS TECNOLOGIAS</h4>
+            <div className="flex gap-6">
+              <HTML width={60} height={60} />
+              <CSSColorido width={60} height={60} />
+              <JavaScript width={60} height={60} />
+              <GitColorido width={60} height={60} />
+            </div>
+            <h5 className="font-[metropolislight] text-base md:text-sm lg-text-base">
+              Projeto antigo e não responsivo.
+            </h5>
+          </div>
+        </div>
+        <div className="dark:bg-neutral-800 grid grid-cols-1 md:grid-cols-2 grid-rows-[auto_1fr] gap-x-2 md:gap-x-8 gap-y-2 bg-neutral-50 px-8 md:px-24 py-8">
+          <h3 className="row-start-1 row-end-2 col-start-1 col-end-2 text-3xl">
+            Painel de Administrador
           </h3>
           <div className="h-fit">
             <a href="https://restaurant-page-eight.vercel.app/" target="_blank">
@@ -243,7 +294,7 @@ export const TodosProjetos = () => {
         </div>
         <div className="dark:bg-neutral-800 grid grid-cols-1 md:grid-cols-2 grid-rows-[auto_1fr] gap-x-2 md:gap-x-8 gap-y-2 bg-neutral-50 px-8 md:px-24 py-8">
           <h3 className="row-start-1 row-end-2 col-start-1 col-end-2 text-3xl">
-            Sign Up Form
+            Formulário de Inscrição
           </h3>
           <div className="h-fit">
             <a href="https://mellow-wisp-41dcac.netlify.app/" target="_blank">
@@ -278,6 +329,33 @@ export const TodosProjetos = () => {
                 className="min-h-[140px] min-w-[280px] object-center object-cover rounded shadow-[0_3px_3px_0_rgba(0,0,0,0.25)]"
                 srcSet="
                   /imagens/cc.png 600w,
+                "
+              />
+            </a>
+          </div>
+          <div className="flex flex-col self-start md:col-start-2 md:col-end-3 md:row-start-1 md:row-end-3 gap-4">
+            <h4 className="text-xl pt-3">PRINCIPAIS TECNOLOGIAS</h4>
+            <div className="flex gap-6">
+              <HTML width={60} height={60} />
+              <CSSColorido width={60} height={60} />
+              <JavaScript width={60} height={60} />
+              <GitColorido width={60} height={60} />
+            </div>
+            <h5 className="font-[metropolislight] text-base md:text-sm lg-text-base">
+              Projeto antigo e não responsivo.
+            </h5>
+          </div>
+        </div>
+        <div className="dark:bg-neutral-800 grid grid-cols-1 md:grid-cols-2 grid-rows-[auto_1fr] gap-x-2 md:gap-x-8 gap-y-2 bg-neutral-50 px-8 md:px-24 py-8">
+          <h3 className="row-start-1 row-end-2 col-start-1 col-end-2 text-3xl">
+            Jogo da Velha
+          </h3>
+          <div className="h-fit">
+            <a href="https://silver-starlight-3d47e3.netlify.app/" target="_blank">
+              <img
+                className="min-h-[140px] min-w-[280px] object-center object-cover rounded shadow-[0_3px_3px_0_rgba(0,0,0,0.25)]"
+                srcSet="
+                  /imagens/tc.png
                 "
               />
             </a>
