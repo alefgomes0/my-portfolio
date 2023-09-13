c4c2a70d70de702eebf396299af1baa0dc5158f7
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-09-13 16:45:21 -0300

    Faz pequenos ajustes na UI

--- src/components/Projetos/Projetos.tsx
+++ src/components/Projetos/Projetos.tsx
@@ -17,7 +17,7 @@ export const Projetos = () => {
       <div className="grid grid-cols-1 grid-rows-[min-height_min-height_min-height_auto] gap-x-8 gap-y-16 mt-4">
         <div className="dark:bg-neutral-800 grid grid-cols-1 md:grid-cols-2 grid-rows-[auto_1fr] gap-x-2 md:gap-x-8 gap-y-2 bg-neutral-50 px-8 md:px-24 py-8">
           <h3 className="row-start-1 row-end-2 col-start-1 col-end-2 text-2xl md:text-3xl">
-            Onde está Wally?
+            ONDE ESTÁ WALLY?
           </h3>
           <div className="h-fit">
             <a
