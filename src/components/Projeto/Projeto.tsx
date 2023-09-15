d2f987b64717d5ed36d09a856fa74ff7e35260b8
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-09-15 15:57:55 -0300

    Adiciona o componente Projeto

---
+++ src/components/Projeto/Projeto.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+type ProjetoProps = {
+  title: string;
+  imagePath: string;
+  children: React.ReactNode;
+  textContent: React.ReactNode;
+};
+
+export const Projeto = ({
+  title,
+  children,
+  imagePath,
+  textContent,
+}: ProjetoProps) => {
+  const [zoomIn, setZoomIn] = useState(false);
+
+  return (
+    <div className="dark:bg-neutral-800 grid grid-cols-1 md:grid-cols-2 grid-rows-[auto_1fr] gap-x-2 md:gap-x-8 gap-y-2 bg-slate-200 px-8 md:px-24 py-8">
+      <h3 className="row-start-1 row-end-2 col-start-1 col-end-2 text-2xl md:text-3xl">
+        {title}
+      </h3>
+      <div className="h-fit">
+        <a
+          href="https://where-is-waldo-git-main-alefgomes0.vercel.app/"
+          target="_blank"
+        >
+          <div className="overflow-hidden">
+            <img
+              onMouseEnter={() => setZoomIn(true)}
+              onMouseLeave={() => setZoomIn(false)}
+              className={`min-h-[140px] min-w-[280px] object-center object-cover ${
+                zoomIn ? "scale-110" : ""
+              } transition-transform duration-300 ease-in-out rounded shadow-[0_3px_3px_0_rgba(0,0,0,0.25)]`}
+              srcSet={`
+                ${imagePath}.png 600w,
+                ${imagePath}-s.png 300w
+              `}
+            />
+          </div>
+        </a>
+      </div>
+      <div className="flex flex-col self-start md:col-start-2 md:col-end-3 md:row-start-1 md:row-end-3 gap-4">
+        <h4 className="text-xl pt-3">PRINCIPAIS TECNOLOGIAS</h4>
+        <div className="flex gap-6">{children}</div>
+        {textContent}
+      </div>
+    </div>
+  );
+};
--- src/components/Projetos/Projetos.tsx
+++ src/components/Projetos/Projetos.tsx
@@ -7,6 +7,7 @@ import { TypeScriptColorido } from "../svg/TypeScript";
 import { NodeJSColorido } from "../svg/NodeJSColorido";
 import { Express } from "../svg/Express";
 import { MongoDB } from "../svg/MongoDB";
+import { Projeto } from "../Projeto/Projeto";
 
 export const Projetos = () => {
   return (
@@ -15,27 +16,11 @@ export const Projetos = () => {
         Meus Projetos
       </h2>
       <div className="grid grid-cols-1 grid-rows-[min-height_min-height_min-height_auto] gap-x-8 gap-y-16 mt-4">
-        <div className="dark:bg-neutral-800 grid grid-cols-1 md:grid-cols-2 grid-rows-[auto_1fr] gap-x-2 md:gap-x-8 gap-y-2 bg-slate-200 px-8 md:px-24 py-8">
-          <h3 className="row-start-1 row-end-2 col-start-1 col-end-2 text-2xl md:text-3xl">
-            ONDE ESTÁ WALLY?
-          </h3>
-          <div className="h-fit">
-            <a
-              href="https://where-is-waldo-git-main-alefgomes0.vercel.app/"
-              target="_blank"
-            >
-              <img
-                className="min-h-[140px] min-w-[280px] object-center object-cover rounded shadow-[0_3px_3px_0_rgba(0,0,0,0.25)]"
-                srcSet="
-                    /imagens/ww.png 600w,
-                    /imagens/ww-s.png 300w
-                  "
-              />
-            </a>
-          </div>
-          <div className="flex flex-col self-start md:col-start-2 md:col-end-3 md:row-start-1 md:row-end-3 gap-4">
-            <h4 className="text-xl pt-3">PRINCIPAIS TECNOLOGIAS</h4>
-            <div className="flex gap-6">
+        <Projeto
+          title="ONDE ESTÁ WALLY?"
+          imagePath="/imagens/ww"
+          children={
+            <>
               <ReactColorido width={60} height={60} />
               <TailwindColorido width={60} height={60} />
               <TypeScriptColorido width={60} height={60} />
@@ -43,182 +28,161 @@ export const Projetos = () => {
               <Express width={60} height={60} />
               <MongoDB width={60} height={60} />
               <GitColorido width={60} height={60} />
-            </div>
-            <h5 className="font-[metropolislight] text-base md:text-sm lg-text-base">
-              Baseado no jogo "Onde está Wally", o backend do site, feito com
-              ExpressJS, utiliza{" "}
-              <span className="font-[metropolisbold] dark:text-white">
-                REST API's{" "}
-              </span>
-              para exibir os pokémons a serem localizados, bem como para mostrar
-              o ranking dos jogadores em cada mapa. Os dados estão armazenados
-              no{" "}
-              <span className="font-[metropolisbold] dark:text-white">
-                MongoDB{" "}
-              </span>
-              e, para manejá-los, foi utilizado o ODM{" "}
-              <span className="font-[metropolisbold] dark:text-white">
-                Mongoose
-              </span>
-              , assim como outros middlewares como{" "}
-              <span className="font-[metropolisbold] dark:text-white">
-                express-validator e express-async-handler
-              </span>
-              . O front-end utilizou Vite (React + TypeScript) e diversos hooks como
-              useState, useEffect, useContext e hooks customizáveis, além de axios para fazer
-              os requests. Já para manejar as rotas internas, a escolha foi o react-router-dom.
-            </h5>
-          </div>
-        </div>
-        <div className="dark:bg-neutral-800 grid grid-cols-1 md:grid-cols-2 grid-rows-[auto_1fr] gap-x-2 md:gap-x-8 gap-y-2 bg-neutral-50 px-8 md:px-24 py-8">
-          <h3 className="row-start-1 row-end-2 col-start-1 col-end-2 text-2xl md:text-3xl">
-            LIVRARIA
-          </h3>
-          <div className="h-fit">
-            <a
-              href="https://shopping-cart-psi-seven.vercel.app/"
-              target="_blank"
-            >
-              <img
-                className="min-h-[140px] min-w-[280px] object-center object-cover rounded shadow-[0_3px_3px_0_rgba(0,0,0,0.25)]"
-                srcSet="
-                    /imagens/sc.png 600w,
-                    /imagens/sc-s.png 300w
-                  "
-              />
-            </a>
-          </div>
-          <div className="flex flex-col self-start md:col-start-2 md:col-end-3 md:row-start-1 md:row-end-3 gap-4">
-            <h4 className="text-xl pt-3">PRINCIPAIS TECNOLOGIAS</h4>
-            <div className="flex gap-6">
+            </>
+          }
+          textContent={
+            <>
+              <h5 className="font-[metropolislight] text-base md:text-sm lg-text-base">
+                Baseado no jogo "Onde está Wally", o backend do site, feito com
+                ExpressJS, utiliza{" "}
+                <span className="font-[metropolisbold] dark:text-white">
+                  REST API's{" "}
+                </span>
+                para exibir os pokémons a serem localizados, bem como para
+                mostrar o ranking dos jogadores em cada mapa. Os dados estão
+                armazenados no{" "}
+                <span className="font-[metropolisbold] dark:text-white">
+                  MongoDB{" "}
+                </span>
+                e, para manejá-los, foi utilizado o ODM{" "}
+                <span className="font-[metropolisbold] dark:text-white">
+                  Mongoose
+                </span>
+                , assim como outros middlewares como{" "}
+                <span className="font-[metropolisbold] dark:text-white">
+                  express-validator e express-async-handler
+                </span>
+                . O front-end utilizou Vite (React + TypeScript) e diversos
+                hooks como useState, useEffect, useContext e hooks
+                customizáveis, além de axios para fazer os requests. Já para
+                manejar as rotas internas, a escolha foi o react-router-dom.
+              </h5>
+            </>
+          }
+        />
+        <Projeto
+          title="LIVRARIA"
+          imagePath="/imagens/sc"
+          children={
+            <>
               <ReactColorido width={60} height={60} />
               <CSSColorido width={60} height={60} />
               <TypeScriptColorido width={60} height={60} />
               <GitColorido width={60} height={60} />
-            </div>
-            <h5 className="font-[metropolislight] text-base md:text-sm lg-text-base">
-              Site com design responsivo que utiliza uma base de dados em
-              arquivo{" "}
-              <span className="font-[metropolisbold] dark:text-white">
-                .json{" "}
-              </span>
-              localizada na própria pasta do projeto para exibir os livros. É
-              possível filtrar por nome ou gênero, adicionar/remover do carrinho
-              de compras e visualizar os itens em uma página para finalizar o
-              pagamento. Os principais hooks do React foram utilizados no
-              projeto, como{" "}
-              <span className="font-[metropolisbold] dark:text-white">
-                useState
-              </span>
-              ,{" "}
-              <span className="font-[metropolisbold] dark:text-white">
-                useEffect
-              </span>
-              ,{" "}
-              <span className="font-[metropolisbold] dark:text-white">
-                useRef
-              </span>
-              ,{" "}
-              <span className="font-[metropolisbold] dark:text-white">
-                useContext
-              </span>{" "}
-              e um{" "}
-              <span className="font-[metropolisbold]">hook personalizado</span>{" "}
-              para armazenar os livros escolhidos em local storage.
-            </h5>
-          </div>
-        </div>
-        <div className="dark:bg-zinc-800 bg-slate-200 grid grid-cols-1 md:grid-cols-2 grid-rows-[auto_1fr] gap-x-2 md:gap-x-8 gap-y-2 bg-neutral-50 px-8 md:px-24 py-8">
-          <h3 className="row-start-1 row-end-2 col-start-1 col-end-2 text-3xl">
-            ONG AMBIENTAL
-          </h3>
-          <div className="h-fit">
-            <a href="https://trees-lilac.vercel.app/" target="_blank">
-              <img
-                className="min-h-[140px] min-w-[280px] object-center object-cover rounded shadow-[0_3px_3px_0_rgba(0,0,0,0.25)]"
-                srcSet="
-                    /imagens/trees.png 600w,
-                    /imagens/trees-s.png 300w
-                  "
-              />
-            </a>
-          </div>
-          <div className="flex flex-col self-start md:col-start-2 md:col-end-3 md:row-start-1 md:row-end-3 gap-4">
-            <h4 className="text-xl pt-3">PRINCIPAIS TECNOLOGIAS</h4>
-            <div className="flex gap-6">
+            </>
+          }
+          textContent={
+            <>
+              <h5 className="font-[metropolislight] text-base md:text-sm lg-text-base">
+                Site com design responsivo que utiliza uma base de dados em
+                arquivo{" "}
+                <span className="font-[metropolisbold] dark:text-white">
+                  .json{" "}
+                </span>
+                localizada na própria pasta do projeto para exibir os livros. É
+                possível filtrar por nome ou gênero, adicionar/remover do
+                carrinho de compras e visualizar os itens em uma página para
+                finalizar o pagamento. Os principais hooks do React foram
+                utilizados no projeto, como{" "}
+                <span className="font-[metropolisbold] dark:text-white">
+                  useState
+                </span>
+                ,{" "}
+                <span className="font-[metropolisbold] dark:text-white">
+                  useEffect
+                </span>
+                ,{" "}
+                <span className="font-[metropolisbold] dark:text-white">
+                  useRef
+                </span>
+                ,{" "}
+                <span className="font-[metropolisbold] dark:text-white">
+                  useContext
+                </span>{" "}
+                e um{" "}
+                <span className="font-[metropolisbold]">
+                  hook personalizado
+                </span>{" "}
+                para armazenar os livros escolhidos em local storage.
+              </h5>
+            </>
+          }
+        />
+
+        <Projeto
+          title="ONG AMBIENTAL"
+          imagePath="/imagens/trees"
+          children={
+            <>
               <ReactColorido width={60} height={60} />
               <TailwindColorido width={60} height={60} />
               <TypeScriptColorido width={60} height={60} />
               <GitColorido width={60} height={60} />
-            </div>
-            <h5 className="font-[metropolislight] text-base md:text-sm lg-text-base">
-              O site, com temática inspirada em uma ONG que ajuda a combater
-              queimadas, conta com algumas funcionalidades. Por um lado, ele
-              consome duas{" "}
-              <span className="font-[metropolisbold] dark:text-white">
-                API's
-              </span>{" "}
-              diferentes para gerar um mapa com as localizações dos últimos
-              incêndios florestais registrados. Além disso, conta com uma página
-              de notícias, com conteúdo lorem ipsum, para mostrar diferentes
-              estilizações de texto. Feito com Tailwind, o site é responsivo e
-              utiliza as principais funcionalidades do React para estruturar o
-              layout, além da biblioteca{" "}
-              <span className="font-[metropolisbold] dark:text-white">
-                axios
-              </span>{" "}
-              e promessas/catch para fazer o get request.
-            </h5>
-          </div>
-        </div>
-        <div className="dark:bg-neutral-800 grid grid-cols-1 md:grid-cols-2 grid-rows-[auto_1fr] gap-x-2 md:gap-x-8 gap-y-2 bg-neutral-50 px-8 md:px-24 py-8">
-          <h3 className="row-start-1 row-end-2 col-start-1 col-end-2 text-3xl">
-            RESTAURANTE
-          </h3>
-          <div className="h-fit">
-            <a href="https://restaurant-page-eight.vercel.app/" target="_blank">
-              <img
-                className="min-h-[140px] min-w-[280px] object-center object-cover rounded shadow-[0_3px_3px_0_rgba(0,0,0,0.25)]"
-                srcSet="
-                    /imagens/rp.png 600w,
-                    /imagens/rp-s.png 300w
-                  "
-              />
-            </a>
-          </div>
-          <div className="flex flex-col self-start md:col-start-2 md:col-end-3 md:row-start-1 md:row-end-3 gap-4">
-            <h4 className="text-xl pt-3">PRINCIPAIS TECNOLOGIAS</h4>
-            <div className="flex gap-6">
+            </>
+          }
+          textContent={
+            <>
+              <h5 className="font-[metropolislight] text-base md:text-sm lg-text-base">
+                O site, com temática inspirada em uma ONG que ajuda a combater
+                queimadas, conta com algumas funcionalidades. Por um lado, ele
+                consome duas{" "}
+                <span className="font-[metropolisbold] dark:text-white">
+                  API's
+                </span>{" "}
+                diferentes para gerar um mapa com as localizações dos últimos
+                incêndios florestais registrados. Além disso, conta com uma
+                página de notícias, com conteúdo lorem ipsum, para mostrar
+                diferentes estilizações de texto. Feito com Tailwind, o site é
+                responsivo e utiliza as principais funcionalidades do React para
+                estruturar o layout, além da biblioteca{" "}
+                <span className="font-[metropolisbold] dark:text-white">
+                  axios
+                </span>{" "}
+                e promessas/catch para fazer o get request.
+              </h5>
+            </>
+          }
+        />
+
+        <Projeto
+          title="RESTAURANTE"
+          imagePath="/imagens/rp"
+          children={
+            <>
               <ReactColorido width={60} height={60} />
               <CSSColorido width={60} height={60} />
               <TypeScriptColorido width={60} height={60} />
               <GitColorido width={60} height={60} />
-            </div>
-            <h5 className="font-[metropolislight] text-base md:text-sm lg-text-base">
-              Projeto feito para solidificar os conhecimentos adquiridos até
-              então. Implementa alguns padrões básicos do front-end como{" "}
-              <span className="font-[metropolisbold] dark:text-white">
-                carrossel de imagens
-              </span>
-              ,{" "}
-              <span className="font-[metropolisbold] dark:text-white">
-                transições
-              </span>{" "}
-              e <span className="font-[metropolisbold]">animações</span>. Em
-              termos visuais, a tentativa foi de criar um design simples,
-              responsivo e elegante com a temática de um restaurante de frutos
-              do mar. Conceitos de React como{" "}
-              <span className="font-[metropolisbold] dark:text-white">
-                props
-              </span>{" "}
-              e{" "}
-              <span className="font-[metropolisbold] dark:text-white">
-                hooks
-              </span>{" "}
-              foram utilizados em todo o projeto.
-            </h5>
-          </div>
-        </div>
+            </>
+          }
+          textContent={
+            <>
+              <h5 className="font-[metropolislight] text-base md:text-sm lg-text-base">
+                Projeto feito para solidificar os conhecimentos adquiridos até
+                então. Implementa alguns padrões básicos do front-end como{" "}
+                <span className="font-[metropolisbold] dark:text-white">
+                  carrossel de imagens
+                </span>
+                ,{" "}
+                <span className="font-[metropolisbold] dark:text-white">
+                  transições
+                </span>{" "}
+                e <span className="font-[metropolisbold]">animações</span>. Em
+                termos visuais, a tentativa foi de criar um design simples,
+                responsivo e elegante com a temática de um restaurante de frutos
+                do mar. Conceitos de React como{" "}
+                <span className="font-[metropolisbold] dark:text-white">
+                  props
+                </span>{" "}
+                e{" "}
+                <span className="font-[metropolisbold] dark:text-white">
+                  hooks
+                </span>{" "}
+                foram utilizados em todo o projeto.
+              </h5>
+            </>
+          }
+        />
         <Link to="/projetos">
           <button
             className="text-xl text-slate-50 ml-8 mb-8 md:ml-24 bg-cyan-600 px-4 py-1 rounded shadow-[0_3px_3px_0_rgba(0,0,0,0.25)] hover:shadow-[0_3px_3px_0_rgba(0,0,0,0.25)_inset]"
